63b89ef9f9e7a5ac5e6169dbafb555fb48a3c2d0
DIEGO CORDOVA <d.cordovaratti@gmail.com>
2022-08-09 16:10:25 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+print("\n")
+print("Roses are " + color.title())
+print(plural_noun.title() + " are Blue")
+print("I love " + celebrity.capitalize())
+print("\n")
